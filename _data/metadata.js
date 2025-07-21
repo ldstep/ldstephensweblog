8c029bd9afaf902e6f862fdb24bd19b673e80479
Loren Stephens <ldstep@ldstephens.com>
2025-07-21 15:16:24 -0400

    First commit

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+   title: "ldstephens weblog",
+   url: "https://weblog.ldstephens.net/",
+   language: "en",
+   description: "A blog by ldstephens",
+   author: {
+      name: "Loren Stephens",
+      email: "",
+      url: "",
+   },
+};
